d071b73afde9143fb6b7d81355afe1161b47fa4d
PHIDELIST <phidelisoluoch@gmail.com>
2023-07-07 11:12:15 +0300

    wip

--- serverless-thumbing-cdk/bin/serverless-thumbing-cdk.ts
+++ serverless-thumbing-cdk/bin/serverless-thumbing-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ServerlessThumbingCdkStack } from '../lib/serverless-thumbing-cdk-stack';
+import { ServerlessImageProcessingCdkStack } from '../lib/serverless-thumbing-cdk-stack';
 
 const app = new cdk.App();
-new ServerlessThumbingCdkStack(app, 'ServerlessThumbingCdkStack', {
+new ServerlessImageProcessingCdkStack(app, 'ServerlessImageProcessingCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
